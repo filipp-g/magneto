a7723e2ae9936af820c1f3e653af2ea362c121a2
chris <cbkrunch17@gmail.com>
2019-10-20 11:41:10 -0400

    Added Reverse Lookup for Values

--- our_tools/interpolation.py
+++ our_tools/interpolation.py
@@ -1,3 +1,5 @@
+import json
+
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D, proj3d
 from scipy.interpolate import griddata
@@ -5,29 +7,36 @@ import matplotlib.pyplot as plt
 import numpy as np
 from our_tools.csaparser import parse_txt
 
-# def rev_lookup(grid, lat, long, time):
-#     return grid[int(lat) - 30][-30 - int(long)][time]
-#
-#
-# def interpol4d():
-#     points = []
-#     lvalues = []
-#     for site, values in full.items():
-#         time_cnt = 0
-#         for time, value in values['data'].items():
-#             points.append([values['lat'], values['long'], time_cnt])
-#             lvalues.append(value)
-#             time_cnt = time_cnt + 1
-#
-#     # We have 12648 points
-#     # long , lat, time
-#     grid_x, grid_y, grid_z = np.mgrid[30:90:60j, -30:-190:160j, 0:743]
-#     points = np.array(points)
-#     lvalues = np.array(lvalues)
-#     grid_z0 = griddata(points, lvalues, (grid_x, grid_y, grid_z), method='linear')
-#
-#     return grid_z0
-#
+grid = np.array(json.loads(open('../data/grid-cache.txt').read()))
+
+
+def estimate_value(lat, long, time):
+    return grid[int(lat) - 30][-30 - int(long)][time]
+
+
+# full = parse_txt('../data/dataset2_full.txt')
+full = {}
+
+
+def interpol4d():
+    points = []
+    lvalues = []
+    for site, values in full.items():
+        time_cnt = 0
+        for time, value in values['data'].items():
+            points.append([values['lat'], values['long'], time_cnt])
+            lvalues.append(value)
+            time_cnt = time_cnt + 1
+
+    # We have 12648 points
+    # long , lat, time
+    grid_x, grid_y, grid_z = np.mgrid[30:90:60j, -30:-190:160j, 0:743]
+    points = np.array(points)
+    lvalues = np.array(lvalues)
+    grid_z0 = griddata(points, lvalues, (grid_x, grid_y, grid_z), method='nearest')
+    f = open('grid-cache.txt', 'w')
+    f.write(json.dumps(grid_z0.tolist()))
+
 #
 # def interpol3d(time):
 #     points = []
