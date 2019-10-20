9e246a0aed082e5b2dc17ec19c3533ce4fc25831
chris <cbkrunch17@gmail.com>
2019-10-20 14:27:10 -0400

    heatmap

--- static/js/script.js
+++ static/js/script.js
@@ -4,47 +4,59 @@ let circles = [];
 let num_sites = 0;
 let total_activity = 0;
 
-var grid = '';
+var grid = "";
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 59.991699, lng: -101.407434},
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 59.991699, lng: -101.407434 },
         zoom: 3.2,
         fullscreenControl: false,
         streetViewControl: false,
         mapTypeControl: false,
-        scaleControl: true,
+        scaleControl: true
     });
     createMarkers(0);
 }
 
 function saveImg() {
-
     var zoom = map.zoom;
     var centre = map.getBounds().getCenter();
     var spherical = google.maps.geometry.spherical;
     bounds = map.getBounds();
     var cor1 = bounds.getNorthEast();
     var cor2 = bounds.getSouthWest();
-    var cor3 = new google.maps.LatLng(cor2.lat(), cor1.lng()); 
+    var cor3 = new google.maps.LatLng(cor2.lat(), cor1.lng());
     var cor4 = new google.maps.LatLng(cor1.lat(), cor2.lng());
 
     var width = distanceInPx(cor1, cor4);
     var height = distanceInPx(cor1, cor3);
 
-    var imgUrl = "https://maps.googleapis.com/maps/api/staticmap?center=" + 
-        centre.lat() + "," + centre.lng() + "&zoom=" + zoom + 
-        "&size=" + width + "x" + height + "&maptype=terrain&key=AIzaSyDLieeiefNfkqBmCwm0FMLiQiXhqTM8p_k"; 
-    
+    var imgUrl =
+        "https://maps.googleapis.com/maps/api/staticmap?center=" +
+        centre.lat() +
+        "," +
+        centre.lng() +
+        "&zoom=" +
+        zoom +
+        "&size=" +
+        width +
+        "x" +
+        height +
+        "&maptype=terrain&key=AIzaSyDLieeiefNfkqBmCwm0FMLiQiXhqTM8p_k";
+
     for (let i = 0; i < markers.length; i++) {
-        imgUrl += "&markers=color:red|" + markers[i].getPosition().lat() + "," + markers[i].getPosition().lng();
+        imgUrl +=
+            "&markers=color:red|" +
+            markers[i].getPosition().lat() +
+            "," +
+            markers[i].getPosition().lng();
     }
 
     imgUrl += ".jpg";
 
-    var link = document.getElementById('staticLink');
+    var link = document.getElementById("staticLink");
     link.setAttribute("href", imgUrl);
-    link.style.display="block"; 
+    link.style.display = "block";
 }
 
 function distanceInPx(pos1, pos2) {
@@ -53,14 +65,15 @@ function distanceInPx(pos1, pos2) {
 
     var pixelSize = Math.pow(2, -map.getZoom());
 
-    var d = Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y))/pixelSize;
+    var d =
+        Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y)) /
+        pixelSize;
 
     return Math.round(d);
 }
 
-
 function heatMapMaker(lat, lng, time) {
-    return (grid[lat - 30][-30 - lng][time]);
+    return grid[parseInt(lat) - 30][-30 - parseInt(lng)][parseInt(time)];
 }
 
 function createMarkers(day) {
@@ -68,17 +81,24 @@ function createMarkers(day) {
     let infowindow = new google.maps.InfoWindow();
     var dayStr = intToDate(day);
     var heatMapData = [];
-    if(grid!='') {
-        for (var lat = 30; lat < 90; lat++) {
-            for (var lng = -30; lng > -190; lng--) {
-                heatMapData.push({location: new google.maps.LatLng(lat, lng), weight: heatMapMaker(lat, lng, day)});
-            }
-        }
-    }
+
 
     for (let key in magneto_json) {
+        if (grid != "") {
+            heatMapData.push({
+                location: new google.maps.LatLng(
+                    magneto_json[key]["lat"],
+                    magneto_json[key]["long"]
+                ),
+                weight: heatMapMaker(magneto_json[key]["lat"],
+                    magneto_json[key]["long"], day)
+            });
+        }
         let marker = new google.maps.Marker({
-            position: new google.maps.LatLng(magneto_json[key]["lat"], magneto_json[key]["long"]),
+            position: new google.maps.LatLng(
+                magneto_json[key]["lat"],
+                magneto_json[key]["long"]
+            ),
             icon: {
                 path: google.maps.SymbolPath.CIRCLE,
                 scale: 0
@@ -87,34 +107,42 @@ function createMarkers(day) {
         });
         let circle = new google.maps.Circle({
             map: map,
-            radius: magneto_json[key]["data"][dayStr] * 1000,    // 10 miles in metres
-            fillColor: 'red',
-            fillOpacity: .2,
-            strokeColor: 'white',
-            strokeWeight: .5
+            radius: 1000, // 10 miles in metres
+            fillColor: "black",
+            fillOpacity: 1,
+            strokeColor: "white",
+            strokeWeight: 0.5
         });
-        circle.bindTo('center', marker, 'position');
+        circle.bindTo("center", marker, "position");
 
         heatmap = new google.maps.visualization.HeatmapLayer({
             data: heatMapData,
             map: map,
-            radius: 12,
-            opacity: 0.3
+            radius: 100,
+            opacity: 0.05
         });
-      
-        heatmap.setMap(map);
 
-        google.maps.event.addListener(circle, 'click', (function (marker, i) {
-            return function () {
-                let content = '<div id="infowindow"><h6>' + key + '</h6>'
-                    + '<p>' + 'magnetic field: ' + (magneto_json[key]["data"][dayStr]).toLocaleString()
-                    + '</p>'/*'Location'*/;
-                infowindow.setContent(content);
-                infowindow.open(map, marker);
-                map.panTo(marker.position);
-            }
-        })(marker, num_sites));
-        document.getElementById("staticLink").addEventListener("click", function(){
+
+        google.maps.event.addListener(
+            circle,
+            "click",
+            (function(marker, i) {
+                return function() {
+                    let content =
+                        '<div id="infowindow"><h6>' +
+                        key +
+                        "</h6>" +
+                        "<p>" +
+                        "magnetic field: " +
+                        magneto_json[key]["data"][dayStr].toLocaleString() +
+                        "</p>"; /*'Location'*/
+                    infowindow.setContent(content);
+                    infowindow.open(map, marker);
+                    map.panTo(marker.position);
+                };
+            })(marker, num_sites)
+        );
+        document.getElementById("staticLink").addEventListener("click", function() {
             // document.getElementById("demo").innerHTML = "Hello World";
             saveImg();
         });
@@ -139,7 +167,7 @@ function clearMarkers() {
     circles = [];
 }
 
-Number.prototype.pad = function (size) {
+Number.prototype.pad = function(size) {
     var s = String(this);
     while (s.length < (size || 2)) {
         s = "0" + s;
@@ -151,18 +179,18 @@ function intToDate(x) {
     return Math.floor(x / 24 + 1).pad(2) + "-" + ((x % 24) + 1).pad(2);
 }
 
-$.get('static/js/grid-cache.txt', {}, function(content){
+$.get("static/js/grid-cache.txt", {}, function(content) {
     grid = JSON.parse(content);
 });
 
 // Doesnt not wait for user to release mouse
-$(document).on("input", "#map-date-slider", function (e) {
+$(document).on("input", "#map-date-slider", function(e) {
     let date = intToDate(e.target.value);
     $("#map-current-date").text(date);
 });
 
 // Waits for user to release mouse
-$(document).on("change", "#map-date-slider", function (e) {
+$(document).on("change", "#map-date-slider", function(e) {
     // let date = intToDate(e.target.value);
     createMarkers(e.target.value);
 });
@@ -171,7 +199,7 @@ function setAverageActivity() {
     $("#map-average-activity").text((total_activity / num_sites).toFixed(4));
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
     let slider = $("#map-date-slider");
     slider[0].value = 0;
     slider.focus();
